05d0743cadb88c538ca12f2e1568d74f721dd360
FrantisekSvatos <147390923+FrantisekSvatos@users.noreply.github.com>
2024-02-22 19:50:13 +0100

    pekarnicka

--- main.ts
+++ main.ts
@@ -22,8 +22,28 @@ let data: Array<Odber> = [
     { cas: 800, spotreba: 1500 },
 ]
 
-for (const peceme of data){
-    console.logValue("čas: ", peceme.cas);
-    console.logValue("spotřeba: ", peceme.spotreba);
-    console.logValue("spotřebováno: ", peceme.spotreba / 3600 * peceme.cas);
+let hodiny: number = 0;
+for (let i = 0; i < data.length; i++) {
+    hodiny += data[i].cas
+    if (i === 4) {
+        console.log((hodiny /= 3600) + " hodiny")
+    }
+}
+
+let kwatty: number = 0;
+for (let o = 0; o < data.length; o++) {
+    kwatty += data[o].spotreba
+    if (o === 4) {
+        console.log((kwatty /= 1000) + " kilowatty")
+    }
+}
+
+console.log((hodiny *= kwatty *= cena) + " Kč")
+
+let prumer: number = 0;
+for (let p = 0; p < data.length; p++) {
+    prumer += data[p].cas / 3600 + data[p].spotreba / 1000
+    if (p === 4) {
+        console.log("průměrný odběr " + (prumer /= 5) + " kWh")
+    }
 }
